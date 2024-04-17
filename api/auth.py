f07fce4214fd848c0019c5d515c19d18c0aa606d
gioheex <giovanni.heex@gmail.com>
2024-04-17 17:06:44 -0300

    rota para verificar email

--- api/auth.py
+++ api/auth.py
@@ -1,7 +1,9 @@
 from flask import render_template, request, Flask, Blueprint, flash, url_for, redirect, session
 from DAO import CheckCadastro, CheckLogin, selectFromWhere, insertCadastro
 from hashlib import sha256
+from envio_email import gerar_codigo, enviar_email
 
+codigos_de_verificacao = {}
 
 auth = Blueprint("auth", __name__, template_folder="templates")
 app = Flask(__name__)
@@ -67,11 +69,37 @@ def registro_post():
     ):
         flash("Preencha o formulário todo!")
         return redirect(url_for("cadastro"))
+
     
     else:
+        # fazer a checagem por código de verificação
+        codigo_verificacao = gerar_codigo()
+        codigos_de_verificacao[email] = codigo_verificacao
+        enviar_email(nome1, codigo_verificacao, email)
+        
         senhaHash = sha256(senha.encode("utf-8")).hexdigest()
         insertCadastro(email, senhaHash, nome1, nome2, cpf)
         flash("Cadastrado com sucesso!")
-        return redirect(url_for("auth.login"))
-        
+        return redirect(url_for("auth.login"))        
 
+@auth.route("/verificar_email")
+def cadastro():
+    return render_template("verificar_email.html")
+
+@auth.route("/verificar_email", methods=["POST"])
+def verificar_email(email):
+    
+    if request.method == "POST":
+        codigo_inserido = request.form.get("codigo")  # Obtém o código de verificação inserido pelo usuário
+
+        
+        # Verifica se o código inserido pelo usuário corresponde ao código enviado por e-mail
+        codigo_gerado = codigos_de_verificacao.get(email)
+        if codigo_gerado and codigo_inserido == codigo_gerado:
+            # Se o código estiver correto, redireciona o usuário para a página de login
+            flash("E-mail verificado com sucesso! Faça login para continuar.")
+            return 
+        else:
+            # Se o código estiver incorreto, exibe uma mensagem de erro e redireciona o usuário de volta para a página de verificação
+            flash("Código de verificação incorreto. Por favor, tente novamente.")
+            return redirect(url_for("auth.verificar_email"))
